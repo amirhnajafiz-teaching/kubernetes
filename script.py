6b1d90bc5bb51e44312983806f2d08001c0b52c8
Official21A <officialamirhossein21@gmail.com>
2020-10-12 07:27:32 +0330

    First build

--- script.py
+++ script.py
@@ -7,6 +7,32 @@ hosts_path = "/etc/hosts"
 redirect = "127.0.0.1"
   
 # websites That you want to block 
-website_list = 
-["www.facebook.com","facebook.com", 
-      "dub119.mail.live.com"] 
+website_list = ["www.facebook.com","facebook.com","dub119.mail.live.com"] 
+
+# program main loop
+while True: 
+
+    # time of your work 
+    if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() and dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,16): 
+        print("Working hours...") 
+        with open(hosts_path, 'r+') as file: 
+            content = file.read() 
+            for website in website_list: 
+                if website in content: 
+                    pass
+                else: 
+                    # mapping hostnames to your localhost IP address 
+                    file.write(redirect + " " + website + "\n") 
+    else: 
+        with open(hosts_path, 'r+') as file: 
+            content=file.readlines() 
+            file.seek(0) 
+            for line in content: 
+                if not any(website in line for website in website_list): 
+                    file.write(line) 
+  
+            # removing hostnmes from host file 
+            file.truncate() 
+  
+        print("Fun hours...") 
+    time.sleep(5) 
