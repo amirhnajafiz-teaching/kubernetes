768d291a0086f6f75d82da97ab0efcb30aa3790d
Official21A <officialamirhossein21@gmail.com>
2020-10-12 07:22:25 +0330

    First build

---
+++ script.py
@@ -0,0 +1,12 @@
+import time 
+from datetime import datetime as dt 
+  
+# change hosts path according to your OS 
+hosts_path = "/etc/hosts"
+# localhost's IP 
+redirect = "127.0.0.1"
+  
+# websites That you want to block 
+website_list = 
+["www.facebook.com","facebook.com", 
+      "dub119.mail.live.com"] 
